4789e44ee6ac240342a4730ae608de9eed06f5cc
Evelyn <17604evelyn@gmail.com>
2023-06-26 15:14:32 +0800

    add Course related files

--- src/main/java/app/com/course/repository/CourseRepository.java
+++ src/main/java/app/com/course/repository/CourseRepository.java
@@ -4,4 +4,8 @@ import app.com.course.vo.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Integer> {
+
+    Course getReferenceByCourseName(String courseName);
+
+    Course findByCourseName(String courseName);
 }
--- src/main/java/app/com/course/service/CourseService.java
+++ src/main/java/app/com/course/service/CourseService.java
@@ -1,7 +1,13 @@
 package app.com.course.service;
 
+import app.com.course.repository.ChefRepository;
+import app.com.course.repository.CourseOrderRepository2;
 import app.com.course.repository.CourseRepository;
-import app.com.course.vo.Course;
+import app.com.course.vo.*;
+import app.com.emp.repository.EmployeeRepository;
+import app.com.emp.vo.Employee;
+import app.com.member.repository.MemberRepository;
+import app.com.member.vo.Members;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +21,18 @@ public class CourseService {
     @Autowired
     CourseRepository repository;
 
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    CourseOrderRepository2 courseOrderRepository2;
+
+    @Autowired
+    ChefRepository chefRepository;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     public List<Course> getCourse() {
         return repository.findAll();
     }
@@ -26,8 +44,8 @@ public class CourseService {
     }
 
 
-    public void addCourse(Course course) {
-        repository.save(course);
+    public Course addCourse(Course course) {
+        return repository.save(course);
     }
 
     public void delete(Integer courseId) {
@@ -39,11 +57,71 @@ public class CourseService {
     }
 
 
-//    public void delete(Integer courseId) {
-//        repository.deleteById(courseId);
-//    }
-//
-//    public void update(Course course) {
-//        repository.save(course);
-//    }
+    //儲存結帳資料
+    public CourseOrder checkout(CheckoutDTO dto) {
+        CourseOrder courseOrder = new CourseOrder();
+        courseOrder.setMemId(dto.getMemberId());
+        courseOrder.setOrderTotal(dto.getCoursePrice());
+        courseOrder.setCourseId(dto.getCourseId());
+
+        return courseOrderRepository2.save(courseOrder);
+    }
+
+    //SHOW結帳資料
+    public CheckoutDTO getCheckoutData(Integer memberId, Integer courseId) {
+        CheckoutDTO dto = new CheckoutDTO();
+
+        // 從資料庫中根據 memberId 查找會員
+        Members member = memberRepository.findById(memberId).orElse(null);
+        if (member != null) {
+            dto.setMemberId(member.getMemberId());
+            dto.setMemberName(member.getMemberName());
+            dto.setMemberEmail(member.getMemberEmail());
+            dto.setMemberPhone(member.getMemberPhone());
+        }
+
+        // 從資料庫中根據 courseId 查找課程
+        Course course = repository.findById(courseId).orElse(null);
+        if (course != null) {
+            dto.setCourseName(course.getCourseName());
+            dto.setCoursePrice(course.getCoursePrice());
+        }
+
+        return dto;
+    }
+
+
+    public ChefInfoDTO getChefData(Integer empId,
+                                   Integer chefId) {
+        ChefInfoDTO dto = new ChefInfoDTO();
+
+        Employee employee = employeeRepository.findById(empId).orElse(null);
+        if (employee != null) {
+            dto.setEmpId(employee.getEmpId());
+            dto.setEmpName(employee.getEmpName());
+            dto.setEmpClassify(employee.getEmpClassify());
+            dto.setEmpAccount(employee.getEmpAccount());
+            dto.setEmpPhone(employee.getEmpPhone());
+            dto.setEmpHireDate(employee.getEmpHireDate());
+            dto.setEmpMail(employee.getEmpMail());
+        }
+
+        Chef chef = chefRepository.findById(chefId).orElse(null);
+        if (chef != null) {
+            dto.setChefId(chef.getChefId());
+            dto.setChefInfo(chef.getChefInfo());
+        }
+        return dto;
+    }
+
+    public Chef updateChefInfo(Integer chefId, String chefInfo) {
+        Chef chef = chefRepository.findById(chefId).orElse(null);
+        if (chef != null) {
+            chef.setChefInfo(chefInfo);
+            chefRepository.save(chef);
+        }
+        return chef;
+    }
+
 }
+
