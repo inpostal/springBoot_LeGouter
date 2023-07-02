45dbfd2f2df4374276b11df023fb3b91311be5b1
Taroro <lucy.ythsiao@gmail.com>
2023-07-02 22:12:31 +0800

    刪除CourseOrder檔案

--- src/main/java/app/com/CourseOrder/repository/CourseOrderDerailRepository.java
+++
@@ -1,8 +0,0 @@
-package app.com.CourseOrder.repository;
-
-import app.com.CourseOrder.vo.CourseOrderDetail;
-import app.com.coupon.vo.CouponType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseOrderDerailRepository extends JpaRepository<CourseOrderDetail, Integer> {
-}
--- src/main/java/app/com/CourseOrder/repository/CourseOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package app.com.CourseOrder.repository;
-
-import app.com.CourseOrder.vo.CourseOrder;
-import app.com.coupon.vo.CouponType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseOrderRepository extends JpaRepository<CourseOrder, Integer> {
-}
