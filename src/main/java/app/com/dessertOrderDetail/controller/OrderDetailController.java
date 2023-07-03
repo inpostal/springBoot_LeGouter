28bc4ca0b44be81b17bd7507137d4ecaf54f721c
Charlie <e3599933@gmail.com>
2023-07-03 18:01:07 +0800

    優化頁面 目前問題：訂單詳情排版、memorder的篩選器沒辦法有作用、訂單完成的圖片沒辦法顯示、結帳的邏輯、驗證

--- src/main/java/app/com/dessertOrderDetail/controller/OrderDetailController.java
+++ src/main/java/app/com/dessertOrderDetail/controller/OrderDetailController.java
@@ -4,12 +4,15 @@ import app.com.dessertOrderDetail.entity.OrderDetailDTO;
 import app.com.dessertOrderDetail.service.OrderDetailService;
 import app.com.dessertOrders.entity.Orders;
 import app.com.dessertOrders.service.OrdersService;
+import app.com.emp.vo.Employee;
+import app.com.member.vo.Members;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -27,13 +30,42 @@ public class OrderDetailController {
     private OrdersService ordersService;
 
 
+//    @GetMapping("/orderDetail/get")
+//    public String getBackEndOrderDetail(@RequestParam Integer orderId, Model model) {
+//        return getOrderDetail(orderId, model, "/back-end/Dessert/DessertOrderDetail");
+//    }
+//
+//    @GetMapping("/memOrderDetail/get")
+//    public String getFrontEndOrderDetail(@RequestParam Integer orderId, Model model) {
+//        return getOrderDetail(orderId, model, "/front-end/Dessert/DessertMemOrderDetail");
+//    }
+//
+//    private String getOrderDetail(Integer orderId, Model model, String viewName) {
+//        List<OrderDetailDTO> orderDetailDTOList = orderDetailService.getOrderDetailDTOList(orderId);
+//        Orders orders = ordersService.getOrdersById(orderId);
+//
+//        model.addAttribute("orderDetailList", orderDetailDTOList);
+//        model.addAttribute("orders", orders);
+//        return viewName;
+//    }
+
     @GetMapping("/orderDetail/get")
-    public String getBackEndOrderDetail(@RequestParam Integer orderId, Model model) {
+    public String getBackEndOrderDetail(@RequestParam Integer orderId, HttpSession session, Model model) {
+        Employee emp = (Employee) session.getAttribute("emp");
+        if (emp == null) {
+            session.setAttribute("location", "/orders/list");
+            return "redirect:/employee/login";
+        }
         return getOrderDetail(orderId, model, "/back-end/Dessert/DessertOrderDetail");
     }
 
     @GetMapping("/memOrderDetail/get")
-    public String getFrontEndOrderDetail(@RequestParam Integer orderId, Model model) {
+    public String getFrontEndOrderDetail(@RequestParam Integer orderId, HttpSession session, Model model) {
+        Members member = (Members) session.getAttribute("user");
+        if (member == null) {
+            session.setAttribute("location", "/ordersMem/list");
+            return "redirect:/login";
+        }
         return getOrderDetail(orderId, model, "/front-end/Dessert/DessertMemOrderDetail");
     }
 
@@ -45,6 +77,5 @@ public class OrderDetailController {
         model.addAttribute("orders", orders);
         return viewName;
     }
-
 
 }
