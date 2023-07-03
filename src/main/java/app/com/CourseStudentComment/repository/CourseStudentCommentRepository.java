e8a964f82e1146c3e31ef6377d3fa47cc224790a
Evelyn <17604evelyn@gmail.com>
2023-07-03 19:25:31 +0800

    修改課程的蟲

--- src/main/java/app/com/CourseStudentComment/repository/CourseStudentCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package app.com.CourseStudentComment.repository;
-
-import app.com.CourseStudentComment.vo.CourseStudentComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseStudentCommentRepository extends JpaRepository<CourseStudentComment, Integer> {
-}
