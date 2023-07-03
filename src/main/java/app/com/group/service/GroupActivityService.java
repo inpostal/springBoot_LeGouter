d4664ef4c574f7a2d43b631fc1ee38f53ae87ea0
Shan <gsn1431@gmail.com>
2023-07-04 01:19:41 +0800

    砍業障，睡前Commit一下。

--- src/main/java/app/com/group/service/GroupActivityService.java
+++ src/main/java/app/com/group/service/GroupActivityService.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.criteria.CriteriaBuilder.Case;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
