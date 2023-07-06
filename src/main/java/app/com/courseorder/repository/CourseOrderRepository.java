91643777efbb089fb6a118472542408098026c79
Sam(Team Leader) <samhuang0415@gmail.com>
2023-07-06 15:15:19 +0800

    更新CourseOrderRepo幫雅琪添加方法 更新輪播圖controller修改時沒有送圖片也可以修改

--- src/main/java/app/com/courseorder/repository/CourseOrderRepository.java
+++ src/main/java/app/com/courseorder/repository/CourseOrderRepository.java
@@ -4,7 +4,9 @@ import app.com.courseorder.vo.CourseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CourseOrderRepository extends JpaRepository<CourseOrder, Integer> {
     List<CourseOrder> findAllByMemId(Integer memberId);
+    Optional<CourseOrder> findByMemIdAndCourseId(Integer memberId, Integer courseId);
 }
